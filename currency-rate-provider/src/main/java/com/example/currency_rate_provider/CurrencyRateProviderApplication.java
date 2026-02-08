8dcfd12e7a9c09880283e482c5acaf756bb4742d
Елена Сметанина <elenasmetanina@MacBook-Pro-de-Elena.local>
2026-02-08 15:45:04 +0300

    Add gRPC currency rate provider and rate printer services

---
+++ currency-rate-provider/src/main/java/com/example/currency_rate_provider/CurrencyRateProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.currency_rate_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRateProviderApplication.class, args);
+	}
+
+}
