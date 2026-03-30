e1da6f4f8681e17382beeea45ffa3c649777c097
Елена Сметанина <elenasmetanina@MacBook-Pro-de-Elena.local>
2026-03-30 13:44:41 +0300

    Add Spring Actuator/Micrometer, Prometheus, Grafana, logging and custom gRPC metrics

--- currency-rate-provider/src/main/java/com/example/currency_rate_provider/CurrencyRateProviderApplication.java
+++ currency-rate-provider/src/main/java/com/example/currency_rate_provider/CurrencyRateProviderApplication.java
@@ -1,13 +1,18 @@
 package com.example.currency_rate_provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CurrencyRateProviderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrencyRateProviderApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(CurrencyRateProviderApplication.class);
 
+    public static void main(String[] args) {
+        log.info("Starting currency-rate-provider version 1.0.0");
+        SpringApplication.run(CurrencyRateProviderApplication.class, args);
+        log.info("currency-rate-provider version 1.0.0 started successfully");
+    }
 }
